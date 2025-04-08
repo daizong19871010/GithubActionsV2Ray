91991c2ecaa05bb25cca0efedd8d28e5f59fa234
shoguncao <shoguncao@tencent.com>
2025-04-08 10:03:16 +0800

    update version

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 2025-04-08 00:56:00
+# 2025-04-09 10:02:00
 # 得到的节点形如：
 # vless://160f2a90-9f87-4452-b27a-e4c03341c138@43.135.118.188:5774?flow=&security=tls&encryption=none&type=ws&host=43.135.118.188&path=/articles&sni=43.135.118.188&fp=chrome&pbk=&sid=&serviceName=/articles&headerType=&mode=&seed=#new server
 
