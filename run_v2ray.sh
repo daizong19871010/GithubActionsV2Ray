5cd3b613d80b352360c7856f8a60de7835205221
shoguncao <shoguncao@tencent.com>
2025-04-08 11:53:43 +0800

    update version，加入niping2346

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 2025-04-09 11:44:00
+# 2025-04-09 11:52:00
 # 得到的节点形如：
 # vless://160f2a90-9f87-4452-b27a-e4c03341c138@43.135.118.188:5774?flow=&security=tls&encryption=none&type=ws&host=43.135.118.188&path=/articles&sni=43.135.118.188&fp=chrome&pbk=&sid=&serviceName=/articles&headerType=&mode=&seed=#new server
 
@@ -38,7 +38,8 @@ pushd frp_0.61.0_linux_amd64
 # 修改frp映射端口
 account_port_map="dahongni21 5774\n"\
 "qinming19871010 5775\n"\
-"shoguncao 5776"
+"niping2346 5776\n"\
+"shoguncao 5777"
 export port=$(echo -e ${account_port_map} | grep ${GITHUB_REPOSITORY_OWNER} | awk '{print $2}')
 perl -pi -e "s/(remotePort = )(.*)($)/\1\$ENV{port}\3/g" frpc.toml
 # frp有点坑，不同起相同的任务名字，所以按时间生成一个名字，然后修改到frpc.toml中
