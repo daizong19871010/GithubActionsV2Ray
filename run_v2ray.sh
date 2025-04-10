5f67c07c673427baf943f66bac512bc402f7c622
caoshougang <caoshougang@caoshougangdeMacBook-Pro.local>
2025-04-10 22:42:49 +0800

    update version

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 2025-04-10 13:19:00
+# 2025-04-10 22:42:00
 # 得到的节点形如：
 # vless://160f2a90-9f87-4452-b27a-e4c03341c138@43.130.11.12:5774?flow=&security=tls&encryption=none&type=ws&host=43.130.11.12&path=/articles&sni=43.130.11.12&fp=chrome&pbk=&sid=&serviceName=/articles&headerType=&mode=&seed=#new server
 
