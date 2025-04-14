8b10b5f976e6c887c0d827e46d13f56012d9b096
shoguncao <shoguncao@tencent.com>
2025-04-14 19:20:37 +0800

    update version

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 2025-04-14 15:07:00
+# 2025-04-14 19:20:00
 # 得到的节点形如：
 # vless://160f2a90-9f87-4452-b27a-e4c03341c138@www.visa.com.sg:443?flow=&security=tls&encryption=none&type=ws&host=monero.keyso.uk&path=/githubactions&sni=monero.keyso.uk&fp=chrome&pbk=&sid=&serviceName=/githubactions&headerType=&mode=&seed=#xray_tunnel
 # ssh登录方式
