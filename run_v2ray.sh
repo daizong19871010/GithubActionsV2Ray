dc80bbf9eb6af12f660978b8c9233e592c64251a
shoguncao <shoguncao@tencent.com>
2025-04-14 14:05:26 +0800

    查看uname -a

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -4,31 +4,8 @@
 # 得到的节点形如：
 # vless://160f2a90-9f87-4452-b27a-e4c03341c138@www.visa.com.sg:443?flow=&security=tls&encryption=none&type=ws&host=monero.keyso.uk&path=/githubactions&sni=monero.keyso.uk&fp=chrome&pbk=&sid=&serviceName=/githubactions&headerType=&mode=&seed=#xray_tunnel
 
-sudo apt update
-sudo apt install openssh-server
-
-echo "cat /etc/ssh/sshd_config"
-cat /etc/ssh/sshd_config
-
-echo "pwd: $(pwd)"
-sudo mkdir -p /root/.ssh
-sudo -i
-cd /root/.ssh
-sudo ssh-keygen -t rsa -b 4096 -C "tmp" -f tmp -P ""
-sudo chmod 600 tmp
-sudo echo "tmp: $(sudo cat tmp)"
-sudo echo "tmp.pub: $(sudo cat tmp.pub)"
-sudo cat tmp.pub >> authorized_keys
-sudo echo "cat authorized_keys"
-sudo cat authorized_keys
-
-cd /home/runner/work/GithubActionsV2Ray/GithubActionsV2Ray
-
-sudo systemctl start ssh
-echo "lsof -i:22"
-lsof -i:22
-echo "ps -ef | grep ssh"
-ps -ef | grep ssh
+echo "uname -a"
+uname -a
 
 mkdir -p xray
 pushd xray
