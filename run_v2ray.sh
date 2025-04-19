346b48baa42ce5caa92835ae4f08f10327007fe0
shoguncao <shoguncao@tencent.com>
2025-04-19 10:54:21 +0800

    修改ssh登录命令，支持不验证known_hosts

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -4,7 +4,7 @@
 # 得到的节点形如：
 # vless://160f2a90-9f87-4452-b27a-e4c03341c138@www.visa.com.sg:443?flow=&security=tls&encryption=none&type=ws&host=githubactions.keyso.uk&path=/githubactions&sni=githubactions.keyso.uk&fp=chrome&pbk=&sid=&serviceName=/githubactions&headerType=&mode=&seed=#xray_tunnel
 # ssh登录方式
-# ssh -vvv -i tmp -o "ProxyCommand=nc -x 127.0.0.1:1080 %h %p" root@127.0.0.1
+# ssh -i tmp -o "ProxyCommand=nc -x 127.0.0.1:1080 %h %p" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@127.0.0.1
 
 sudo apt-get update
 sudo apt-get install openssh-server
