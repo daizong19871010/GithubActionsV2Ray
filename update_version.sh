8b4075e9db6dc03d33528c1bfb13a1615543f7e7
shoguncao <shoguncao@tencent.com>
2025-04-21 10:47:14 +0800

    完善update_version.sh

--- update_version.sh
+++ update_version.sh
@@ -3,6 +3,10 @@
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 current_time=$(printf "%s" "${current_time}")
 perl -pi -e "s#(^.*)(current_version: )(.*$)#\# current_version: ${current_time}#g" run_v2ray.sh
+git diff
+git add run_v2ray.sh
+git commit -m "update version"
+git push
 
 exit 0
 
