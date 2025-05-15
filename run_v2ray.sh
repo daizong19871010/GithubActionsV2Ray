82dec4d8ab35a863c0cfd54269ae3bc45addb1bf
shoguncao <shou_gang@163.com>
2025-05-15 17:44:04 +0800

    update version

--- run_v2ray.sh
+++ run_v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# current_version: 2025-05-15 17:38:52
+# current_version: 2025-05-15 17:44:04
 # 得到的节点形如：
 # vless://160f2a90-9f87-4452-b27a-e4c03341c138@www.visa.com.sg:443?flow=&security=tls&encryption=none&type=ws&host=githubactions.keyso.uk&path=/githubactions&sni=githubactions.keyso.uk&fp=chrome&pbk=&sid=&serviceName=/githubactions&headerType=&mode=&seed=#xray_tunnel
 # ssh登录方式
