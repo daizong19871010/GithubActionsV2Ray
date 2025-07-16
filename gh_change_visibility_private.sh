778dbd959e9d699c3fe428f7d6ecae2107eee2d4
shoguncao <shoguncao@tencent.com>
2025-07-16 13:01:20 +0800

    减少输出

--- gh_change_visibility_private.sh
+++ gh_change_visibility_private.sh
@@ -29,6 +29,6 @@ get_account_id
 
 command="curl -X PATCH -H 'Authorization: Bearer ${token}' -H 'Accept: application/vnd.github+json' -H 'X-GitHub-Api-Version: 2022-11-28' 'https://api.github.com/repos/${account_id}/${repo_name}' -d '{\"private\": true}'"
 echo "command: ${command}"
-eval "${command}"
+eval "${command}" > /dev/null
 
 exit 0
